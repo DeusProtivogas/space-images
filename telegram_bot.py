41c4d8daa8641b42dd4a805bd678bf5fbdd45556
Kirill <kirill2689@gmail.com>
2023-11-13 12:41:14 +0300

    fixes to argparse

--- telegram_bot.py
+++ telegram_bot.py
@@ -10,21 +10,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Launch Telegram Bot: ')
     parser.add_argument('-p', '--pause', nargs='?', const=3600 * 4, type=int, help='Pause between images in seconds')
     args = parser.parse_args()
-    print(args.pause)
+    if not args.pause:
+        args.pause = 3600 * 4
 
     bot_token = os.environ["TELEGRAM_BOT_TOKEN"]
     chat_id = "@space_images_devman"
 
     bot = telegram.Bot(token=bot_token)
 
-
-    # print(bot.get_me())
-
-    # bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
-    # bot.send_document(chat_id=chat_id, document=open('images/spacex_0.jpeg', 'rb'))
-    # for _, _, item in os.walk("images"):
-    #     print(item)
-    #     print("AAA")
     images = list(os.walk("images"))[0][2]
 
     while True:
