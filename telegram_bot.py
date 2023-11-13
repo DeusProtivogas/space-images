08411106bfd4c43aaca63a863fe95624254e3d0c
Kirill <kirill2689@gmail.com>
2023-11-13 11:54:36 +0300

    publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     chat_id = "@space_images_devman"
 
     bot = telegram.Bot(token=bot_token)
-    print(bot.get_me())
+    # print(bot.get_me())
 
-    bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
+    # bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
+    bot.send_document(chat_id=chat_id, document=open('images/spacex_0.jpeg', 'rb'))
