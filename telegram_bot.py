34ff93ac80e0cd5b46ade569fcc4f1dfe8997225
Kirill <kirill2689@gmail.com>
2023-11-13 11:37:37 +0300

    publish text to Telegram channel

---
+++ telegram_bot.py
@@ -0,0 +1,14 @@
+import os
+
+import telegram
+from dotenv import load_dotenv
+
+if __name__ == "__main__":
+    load_dotenv()
+    bot_token = os.environ["TELEGRAM_BOT_TOKEN"]
+    chat_id = "@space_images_devman"
+
+    bot = telegram.Bot(token=bot_token)
+    print(bot.get_me())
+
+    bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
