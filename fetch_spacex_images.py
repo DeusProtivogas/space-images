3e24135fc8436598a676856ebff34c8eea66e90c
Kirill <kirill2689@gmail.com>
2023-11-14 08:06:04 +0300

    Improved argparse

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -4,29 +4,24 @@ import argparse
 from download_image_util import download_image
 
 
-def get_SpaceX_images(url):
+def get_spacex_images(url):
     response = requests.get(url)
     response.raise_for_status()
-
-    # print(response.json().get("links").get("flickr"))
 
     return response.json().get("links").get("flickr").get("original")
 
 
-def fetch_spacex_launch(path, id):
-    if id:
-        url_spacex = f"https://api.spacexdata.com/v5/launches/{id}"
-    else:
-        url_spacex = "https://api.spacexdata.com/v5/launches/latest"
+def fetch_spacex_launch(path, launch_id):
+    spacex_url = f"https://api.spacexdata.com/v5/launches/{launch_id}"
 
-    links = get_SpaceX_images(url_spacex)
+    links = get_spacex_images(spacex_url)
     for link_index, link in enumerate(links):
         download_image(link, f"{path}spacex_{link_index}.jpeg")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Download SpaceX images: ')
-    parser.add_argument('-i', '--id', help='Launch id')
+    parser.add_argument('-i', '--id', default='latest', help='Launch id')
     args = parser.parse_args()
 
     path = "images/"
